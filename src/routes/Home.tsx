a157fcc1e054d59b0c4d662ae7a36aa549694b13
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-04-10 19:25:49 +0500

    POST request to sign out from an account

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 function Home() {
+  const [loading, setLoading] = useState(false);
+
   const username = localStorage.getItem("username");
+  const token = localStorage.getItem("token");
+
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch("https://node-express-food.vercel.app/auth/signout", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        setLoading(false);
+        alert("Failed to sign out!");
+      }
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      localStorage.removeItem("role");
+      navigate("/signin");
+    } catch (err) {
+      setLoading(false);
+      console.error(err);
+    }
+  };
 
   return (
     <div>
       <p>Welcome, {username}!</p>
+      <button onClick={handleSignOut}>{loading ? "Loading..." : "Sign Out"}</button>
     </div>
   )
 }
